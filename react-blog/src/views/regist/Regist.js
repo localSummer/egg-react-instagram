a3da0a32f383d111a8529e8bbda2ffa12ad96bde
wangxingwen <wangxingwen@liepin.com>
2019-01-09 16:47:52 +0800

    add regist page

---
+++ react-blog/src/views/regist/Regist.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Regist extends Component {
+  render() {
+    return (
+      <div>
+        sign up
+      </div>
+    );
+  }
+}
+
+export default Regist;
