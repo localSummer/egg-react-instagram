6bfa1c5d7d44b0f16de3d49e3d19652cd515a5ec
wangxingwen <wangxingwen@liepin.com>
2019-02-21 17:41:24 +0800

    update

--- react-instagram/src/common/config.js
+++ react-instagram/src/common/config.js
@@ -5,7 +5,7 @@ let redirectUrl = '';
 switch(env) {
   case 'dev':
     baseDomain = '/api/v2';
-    redirectUrl = 'http://127.0.0.1:3000/#/'
+    redirectUrl = 'http://127.0.0.1:3000/#/';
     break;
   case 'prod':
     baseDomain = 'http://127.0.0.1:7001/api/v2';
